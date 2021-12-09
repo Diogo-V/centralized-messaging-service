259a8d2ef6186f8ad260d35b32afae1d37b20b2c
Diogo Venâncio <diogo.venancio00@gmail.com>
2021-12-09 09:28:58 +0000

    Changes by duart

--- server/src/api.cpp
+++ server/src/api.cpp
@@ -13,10 +13,13 @@
  */
 string register_user(unordered_map<string, User>* users, string uid, string pass){
 
-    if (uid == "00000") { /* verifies if user id is not 00000 */
+    if(users->size()==USER_LIMIT){ /* verifies if user limit has been reached */
         return "NOK";
     }
-    else if(users->find(uid) == users->end()){ /* verifies if user isn't already registered */
+    else if(uid=="00000"){ /* verifies if user id is not 0 */
+        return "NOK";
+    }
+    else if(users->find(uid)==users->end()){ /* verifies if user isn't already registered */
         return "DUP";
     }
     else{ /* registers user */
@@ -24,5 +27,59 @@ string register_user(unordered_map<string, User>* users, string uid, string pass
         users->insert(make_pair(uid, user));
         return "OK";
     }
+
 }
 
+/**
+ * @brief Unregisters user
+ *
+ * @param unordered map of users
+ * @param user id
+ * @param user password
+ *
+ * @return status message
+ */
+string unregister_user(unordered_map<string, User>* users, string uid, string pass){
+
+    if(users->find(uid)!=users->end()){ /* verifies if user is already registered */
+        return "NOK";
+    }
+    else if(users->at(uid).getUserPassword()!=pass){ /* verifies if user password is correct */
+        return "NOK";
+    }
+    else{ /* unregisters user */
+        users->erase(uid);
+        return "OK";
+    }
+
+}
+
+/**
+ * @brief user logs in
+ *
+ * @param users structure that holds all users in the server
+ * @param uid user id
+ * @param pass user password
+ * @return
+ */
+string login_user(unordered_map<string, User>* users, string uid, string pass) {
+
+    auto it = users->find(uid);
+
+    /* verifies if user is registered*/
+    if(it == users->end()){
+            return "NOK";
+    }
+
+    /*verifies is password is equal*/
+    else if (it->second.getUserPassword() != pass){
+        return "NOK";
+    }
+
+    /* user logged in*/
+    else{
+        return "OK";
+    }
+}
+
+string list_groups();
--- server/src/api.h
+++ server/src/api.h
@@ -2,9 +2,13 @@
 #define PROJETO_RC_39_V2_API_H
 
 #include "models/user.h"
+#define USER_LIMIT 99999
 
 using namespace std;
 
 string register_user(unordered_map<int, User>* users, string uid, string pass);
+string unregister_user(unordered_map<string, User>* users, string uid, string pass);
+string login_user(unordered_map<string, User>* users, string uid, string pass);
+string list_groups();
 
 #endif //PROJETO_RC_39_V2_API_H
