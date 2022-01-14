d60a63a26125967ad982e788d4847d3fbab6f44c
Sofia-Morgado <sofia.morgado@tecnico.ulisboa.pt>
2022-01-14 13:25:45 +0000

    fixing ulist

--- server/src/models/manager.cpp
+++ server/src/models/manager.cpp
@@ -365,6 +365,10 @@ string Manager::doRegister(const string& input) {
     verbose_(this->getVerbose(), "UID: " + inputs[1] + " | GID: " + inputs[2] + " | IP: " +
         this->getConnection()->getClientIP() + " | PORT: " + this->getConnection()->getClientPort())
 
+    cout << this->getGroups()->empty() << endl;
+    cout << this->getGroups()->count(inputs[1]) << endl;
+    cout << this->getGroups()->at(inputs[1]).getGroupId() << endl;
+
     string status = users_subscribed(this->getGroups(), inputs[1]);
     return "RUL " + status + "\n";
 
